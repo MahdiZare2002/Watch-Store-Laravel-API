29d7a58ee5108d8f1d4556731765270945954d4d
MahdiZare2002 <mahdi.zed2002@gmail.com>
2024-05-05 18:25:06 +0330

    create comment model & migration

---
+++ database/migrations/2024_05_05_145038_create_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->string('body')->nullable();
+            $table->string('status')->default(\App\Enums\CommentStatus::Draft->value);
+            $table->unsignedBigInteger('parent_id')->nullable();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('product_id')->constrained('products');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
